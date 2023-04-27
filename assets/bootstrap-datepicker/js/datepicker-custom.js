5e77f7f586640beaa90244cb0937615949161746
CaoHoangKiet222 <caohoangkiet1720@gmail.com>
2023-04-27 10:20:14 +0700

    [feat]: first commit

---
+++ assets/bootstrap-datepicker/js/datepicker-custom.js
@@ -0,0 +1,27 @@
+$(function() {
+  $("#datepicker").datepicker({
+    autoclose: true,
+    todayHighlight: true,
+  });
+});
+
+$(function() {
+  $("#datepicker2").datepicker({
+    autoclose: true,
+    todayHighlight: true,
+  });
+});
+
+$(function() {
+  $("#datepicker3").datepicker({
+    autoclose: true,
+    todayHighlight: true,
+  });
+});
+
+$(function() {
+  $("#datepicker4").datepicker({
+    autoclose: true,
+    todayHighlight: true,
+  });
+});
